9bab60382086f4e6c02ba91614dc4daf2e212571
mikolaj <mikolajt55@gmail.com>
2025-05-10 09:55:59 +0200

    Added base class

---
+++ Bots/bot.h
@@ -0,0 +1,20 @@
+#ifndef BOT_H
+#define BOT_H
+
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include <memory>
+
+class bot {
+protected:
+    virtual std::vector<std::pair<int, int>> analyzeBoard() = 0;
+    virtual std::vector<std::pair<int, int>> checkForWinningMove();
+public:
+    bot() = default;
+    virtual ~bot() = default;
+
+    virtual void makeMove() = 0;
+};
+
+#endif //BOT_H

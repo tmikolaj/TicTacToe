d38d44de227f7ea10a28c8eaaa5c21f4b6142639
mikolaj <mikolajt55@gmail.com>
2025-05-10 11:16:42 +0200

    Added method for clearing screen, initialized some variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include "Game/GameManager.h"
+#include "Bots/easyBot.h"
+
+void clear();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    srand(static_cast<unsigned>(time(nullptr)));
+    std::vector<std::vector<char>> board(3, std::vector<char>(3, ' '));
+
+    unsigned short turnCount = 0, option = 0;
+    bool isXturn = true;
+    bot* aiBot = nullptr;
+}
+
+void clear() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }

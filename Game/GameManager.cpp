7ee7e79c331c761a20f01ef5d0223412a65ffcc0
TymonModliszka <140974987+TymonModliszka@users.noreply.github.com>
2025-05-10 10:25:45 +0200

    Added GameManager files

---
+++ Game/GameManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by macie on 10.05.2025.
+//
+
+#include "GameManager.h"
---
+++ Game/GameManager.h
@@ -0,0 +1,16 @@
+//
+// Created by macie on 10.05.2025.
+//
+
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+
+
+
+class GameManager {
+
+};
+
+
+
+#endif //GAMEMANAGER_H

9fdb48b8b00a9121869f296ebe942d4b83df9593
mikolaj <mikolajt55@gmail.com>
2025-05-11 19:35:48 +0200

    Set constructor and destructor as default, isXturn is passed by reference now, fixed winCheck definition

--- Game/GameManager.h
+++ Game/GameManager.h
@@ -4,16 +4,15 @@
 #include <iostream>
 #include <vector>
 
-
 class GameManager {
 private:
-    void posCal(unsigned short position, bool isXturn, std::vector<std::vector<char>> board);
-    void insertPos(bool isXturn);
+    void posCal(unsigned short position, bool& isXturn, std::vector<std::vector<char>> board);
+    void insertPos(bool& isXturn);
 public:
-    GameManager();
-    ~GameManager();
-    void playRound(bool isXturn);
-    bool winCheck();
+    GameManager() = default;
+    ~GameManager() = default;
+    void playRound(bool& isXturn);
+    bool winCheck(std::vector<std::vector<char>> board);
 };
 
 

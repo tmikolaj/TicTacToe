3a3e2710a03e79af33d16c937363dcd468084f07
mikolaj <mikolajt55@gmail.com>
2025-05-11 19:42:22 +0200

    Added break and default to switch, added missing "GameManager::" to some functions, changed winCheck

--- Game/GameManager.cpp
+++ Game/GameManager.cpp
@@ -1,14 +1,7 @@
-
+#include <iostream>
 #include "GameManager.h"
 
-GameManager::GameManager() {
-
-}
-GameManager::~GameManager() {
-
-}
-
-void GameManager::playRound(bool isXturn) {
+void GameManager::playRound(bool& isXturn) {
     if (isXturn) {
         std::cout << "X TURN" << std::endl;
     }
@@ -17,8 +10,7 @@ void GameManager::playRound(bool isXturn) {
     }
     insertPos(isXturn);
 }
-
-void GameManager::insertPos(bool isXturn) {
+void GameManager::insertPos(bool& isXturn) {
     unsigned short position;
     if (isXturn) {
         std::cout << "choose position(1 - 9)" << std::endl;
@@ -30,64 +22,81 @@ void GameManager::insertPos(bool isXturn) {
     }
 }
 //Converting Position to Matrix indices
-void posCal(unsigned short position, bool isXturn, std::vector<std::vector<char>> board) {
+void GameManager::posCal(unsigned short position, bool& isXturn, std::vector<std::vector<char>> board) {
     switch (position) {
         case 1:
             if (board[0][0] == ' ') {
                 board[0][0] = isXturn ? 'X' : 'O';
             }
+            break;
         case 2:
             if (board[0][1] == ' ') {
                 board[0][1] = isXturn ? 'X' : 'O';
             }
+            break;
         case 3:
             if (board[0][2] == ' ') {
                 board[0][2] = isXturn ? 'X' : 'O';
             }
+            break;
         case 4:
             if (board[1][0] == ' ') {
                 board[1][0] = isXturn ? 'X' : 'O';
             }
+            break;
         case 5:
             if (board[1][1] == ' ') {
                 board[1][1] = isXturn ? 'X' : 'O';
             }
+            break;
         case 6:
             if (board[1][2] == ' ') {
                 board[1][2] = isXturn ? 'X' : 'O';
             }
+            break;
         case 7:
             if (board[2][0] == ' ') {
                 board[2][0] = isXturn ? 'X' : 'O';
             }
+            break;
         case 8:
             if (board[2][1] == ' ') {
                 board[2][1] = isXturn ? 'X' : 'O';
             }
+            break;
         case 9:
             if (board[2][2] == ' ') {
                 board[2][2] = isXturn ? 'X' : 'O';
             }
+            break;
+        default:
+            isXturn = !isXturn;
+            insertPos(isXturn);
     }
 }
-
-bool winCheck(std::vector<std::vector<char>> board) {
-    for (int i = 0; i < 3; i++) {
-        if ((board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X')  ||  (board[0][i] == 'X' && board[0][i] == 'X' && board[0][i] == 'X')) {
+bool GameManager::winCheck(std::vector<std::vector<char>> board, bool isXturn) {
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+        {
+            std::cout << isXturn ? "O wins!\n" : "X wins!\n";
             return true;
         }
-        else if ((board[i][0] == 'O' && board[i][1] == 'O' && board[i][2] == 'O')  ||  (board[0][i] == 'O' && board[0][i] == 'O' && board[0][i] == 'O')) {
-            return false;
-        }
-    }
-    if (board[1][1] == 'X') {
-        if ((board[0][0] == 'X' && board[2][2] == 'X') || (board[0][2] == 'X' && board[2][0] == 'X')) {
+        if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i])
+        {
+            std::cout << isXturn ? "O wins!\n" : "X wins!\n";
             return true;
         }
     }
-    else if (board[1][1] == 'O') {
-        if ((board[0][0] == 'O' && board[2][2] == 'O') || (board[0][2] == 'O' && board[2][0] == 'O')) {
-            return false;
-        }
+    if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    {
+        std::cout << isXturn ? "O wins!\n" : "X wins!\n";
+        return true;
     }
+    else if (board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    {
+        std::cout << isXturn ? "O wins!\n" : "X wins!\n";
+        return true;
+    }
+    return false;
 }
